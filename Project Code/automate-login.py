6da5a8cb712bd589ad57242e57b2df9d1cc65282
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-07-06 18:01:49 +0600

    Update automate-login.py
    Issue #7

--- Project Code/automate-login.py
+++ Project Code/automate-login.py
@@ -2,11 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox(executable_path='C:\geckodriver.exe')
-driver.get('https://www.facebook.com/')
-emailElement = driver.find_element(By.XPATH, './/*[@id="email"]')
-emailElement.send_keys('maheen@northsouth.edu')
-passElement = driver.find_element(By.XPATH, './/*[@id="pass"]')
-passElement.send_keys('abecoeasd')
 
-elem = driver.find_element(By.XPATH, '//*[@id="u_0_2"]')
-elem.click()
+
+def log_to_facebook():
+    driver.get('https://www.facebook.com/')
+    emailElement = driver.find_element(By.XPATH, './/*[@id="email"]')
+    emailElement.send_keys('maheen@northsouth.edu')
+    passElement = driver.find_element(By.XPATH, './/*[@id="pass"]')
+    passElement.send_keys('abecoeasd')
+    elem = driver.find_element(By.XPATH, '//*[@id="u_0_2"]')
+    elem.click()
+
+
+def log_to_github():
+    driver.get('https://www.facebook.com/')
