bb192e0a32c93c228b50d6f6fa99fb7dc5673fcb
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-07-14 17:02:51 +0600

    Solving Issue #12 finishing the shortcut virus detection and cleaning

--- antivirus.py
+++ antivirus.py
@@ -1,6 +1,13 @@
 import psutil
+import os
 
 # li = psutil.pids()
 # print(li)
 # p = psutil.Process(272)
 # print(p.name())
+os.system('cd C:\\')
+os.system('attrib -h -r -s autorun.inf')
+try:
+    os.system('del autorun.inf')
+except FileNotFoundError:
+    pass
