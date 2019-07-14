000ff687be0960468082c5e9780de4e915595e10
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-07-14 19:37:22 +0600

    Solving Issue #16 checking all process for virus and terminating

--- antivirus.py
+++ antivirus.py
@@ -1,10 +1,15 @@
 import psutil
 import os
+import clamd
 
-# li = psutil.pids()
-# print(li)
-# p = psutil.Process(272)
-# print(p.name())
+programs = psutil.pids()
+virus_check = clamd.ClamdUnixSocket()
+for program in programs:
+    open('{]'.format(program.exe()), 'wb').write(clamd.EICAR)
+    temp = virus_check.scan('{}'.format(program.exe()))
+    if temp[0] == 'FOUND':
+        program.terminate()
+
 os.system('cd C:\\')
 os.system('attrib -h -r -s autorun.inf')
 try:
