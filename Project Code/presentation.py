1ed39324793d2c130329e1f51e05a028c7b50310
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-07-15 05:00:20 +0600

    Solving Issue #14 creating speech recognizer

--- Project Code/presentation.py
+++ Project Code/presentation.py
@@ -1,10 +1,9 @@
 import pyttsx3
-from selenium import webdriver
-import win32com.client
-import time
+# from selenium import webdriver
+# import win32com.client
+# import time
 from pynput.keyboard import Key, Controller
 import speech_recognition as sr
-
 
 keyboard = Controller()
 
@@ -21,3 +20,17 @@ voices = serena.getProperty('voices')
 serena.setProperty('voice', voices[0].id)
 serena.setProperty('rate', 170)
 
+
+def my_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 2
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+        print(command)
+        return command
+
+    except sr.UnknownValueError or TypeError:
+        assistant()
