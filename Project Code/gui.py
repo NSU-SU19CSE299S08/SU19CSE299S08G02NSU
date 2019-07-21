e3bb4665928d1b0efe117f3acf3bbfc3223dd72f
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-07-22 01:14:51 +0600

    Bug Fixing for #18

--- Project Code/gui.py
+++ Project Code/gui.py
@@ -27,6 +27,8 @@ class MainApp(QMainWindow, ui):
         self.pushButton.clicked.connect(self.download_file)
         self.pushButton_3.clicked.connect(self.handle_browse)
         self.pushButton_7.clicked.connect(self.get_video_data)
+        self.pushButton_12.clicked.connect(self.download_video)
+        self.pushButton_6.clicked.connect(self.save_browse_yt)
 
     def handle_progress(self, block_num, block_size, total_size):
         read_data = block_num * block_size
@@ -60,7 +62,12 @@ class MainApp(QMainWindow, ui):
     def save_browse(self):
         pass
 
-    ######################YT downloader##############################
+    #####################YT downloader##############################
+
+    def save_browse_yt(self):
+        save_location = QFileDialog.getSaveFileName(caption='Save as', directory='.', filter="All Files(*.*)")
+        self.lineEdit_8.setText(str(save_location[0]))
+
     def get_video_data(self):
         video_url = self.lineEdit_7.text()
         if video_url == '':
@@ -73,15 +80,32 @@ class MainApp(QMainWindow, ui):
             video_streams = video.videostreams
             for stream in video_streams:
                 size = humanize.naturalsize(stream.get_filesize())
-                if stream.extension =='mp4':
+                if stream.extension == 'mp4':
                     data = "{} {} {} {}".format(stream.mediatype, stream.extension, stream.quality, size)
                     self.comboBox.addItem(data)
 
     def download_video(self):
-        pass
+        video_url = self.lineEdit_7.text()
+        save_location = self.lineEdit_8.text()
+        if video_url == '' or save_location == '':
+            QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
+        else:
+            video = pafy.new(video_url)
+            video_stream = video.videostreams
+            video_quality = self.comboBox.currentIndex()
+            download = video_stream[video_quality].download(filepath=save_location,callback = self.video_progress)
+            QMessageBox.information(self, "Download Completed", "The Download Completed Successfully")
+            QApplication.processEvents()
 
-    def video_progress(self):
-        pass
+
+    def video_progress(self, total, received, time):
+        read_data = received
+        if total > 0:
+            download_percentage = read_data * 100 / total
+            self.progressBar_3.setValue(download_percentage)
+            remaining_time = round(time / 60, 2)
+            self.label_8.setText(str('{} minutes remaining'.format(remaining_time)))
+            QApplication.processEvents()
 
 
 def main():
