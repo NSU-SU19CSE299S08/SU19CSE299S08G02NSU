30d657e00d5c6b67cdcff88ff6aaa0ddbf8545ee
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-08-05 05:38:28 +0600

    Feature #21 creating a basic android app
    In this Issue, I will create a very basic android app.

---
+++ Project Code/Android /app/src/main/java/com/example/clientapp/Client.java
@@ -0,0 +1,8 @@
+package com.example.clientapp;
+
+import android.os.AsyncTask;
+
+public class Client extends AsyncTask<Void,Void,Void>
+{
+
+}
---
+++ Project Code/Android /app/src/main/java/com/example/clientapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.clientapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lowTide(View v)
+    {
+
+    }
+    public void normalTide(View v)
+    {
+
+    }
+    public void faceRecognizer(View v)
+    {
+
+    }
+}
