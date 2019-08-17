008b11ae34461c39311de87a4e21aed8be7873c8
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-08-18 04:02:20 +0600

    Feature #23 creating a function to get the speech to text functionality

--- Project Code/Android App/client_app/app/src/main/java/com/example/client_app/MainActivity.java
+++ Project Code/Android App/client_app/app/src/main/java/com/example/client_app/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.client_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        startActivityForResult(intent, 10);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support This", Toast.LENGTH_SHORT).show();
+        }
     }
 }
