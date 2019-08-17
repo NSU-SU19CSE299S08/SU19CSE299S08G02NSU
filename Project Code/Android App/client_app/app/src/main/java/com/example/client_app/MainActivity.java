426e787f7667f2aba89c480af0526577a3513e08
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-08-18 04:14:07 +0600

    Feature #28 using the Client class inside of Activity

--- Project Code/Android App/client_app/app/src/main/java/com/example/client_app/MainActivity.java
+++ Project Code/Android App/client_app/app/src/main/java/com/example/client_app/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.client_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txvResult = (TextView) findViewById(R.id.textView);
     }
     public void getSpeechInput(View view) {
 
@@ -30,4 +36,21 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Your Device Don't Support This", Toast.LENGTH_SHORT).show();
         }
     }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        switch (requestCode){
+            case 10:
+                if (resultCode == RESULT_OK && data != null){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Client cl = new Client(result.get(0));
+                    cl.execute();
+                    txvResult.setText(result.get(0));
+                }
+                break;
+        }
+    }
+
 }
