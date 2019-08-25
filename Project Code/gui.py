cb46558fcbd056fdf54c6cfb4a4891d07b676789
Syed Mustavi Maheen <mustavi.maheen@northsouth.edu>
2019-08-26 05:39:20 +0600

    another bug fixing

--- Project Code/gui.py
+++ Project Code/gui.py
@@ -9,154 +9,156 @@ import urllib.request
 import pafy
 import humanize
 
-ui, _ = loadUiType('GUI/main.ui')
 
+def open_idm():
+    ui, _ = loadUiType('GUI/main.ui')
 
-class MainApp(QMainWindow, ui):
-    def __init__(self, parent=None):
-        super(MainApp, self).__init__(parent)
-        QMainWindow.__init__(self)
-        self.setupUi(self)
-        self.inint_ui()
-        self.handle_buttons()
+    class MainApp(QMainWindow, ui):
+        def __init__(self, parent=None):
+            super(MainApp, self).__init__(parent)
+            QMainWindow.__init__(self)
+            self.setupUi(self)
+            self.inint_ui()
+            self.handle_buttons()
 
-    def inint_ui(self):
-        pass
+        def inint_ui(self):
+            pass
 
-    def handle_buttons(self):
-        self.pushButton.clicked.connect(self.download_file)
-        self.pushButton_3.clicked.connect(self.handle_browse)
-        self.pushButton_7.clicked.connect(self.get_video_data)
-        self.pushButton_12.clicked.connect(self.download_video)
-        self.pushButton_6.clicked.connect(self.save_browse_yt)
-        self.pushButton_14.clicked.connect(self.playlist_downloader)
-        self.pushButton_13.clicked.connect(self.save_browser_playlist)
+        def handle_buttons(self):
+            self.pushButton.clicked.connect(self.download_file)
+            self.pushButton_3.clicked.connect(self.handle_browse)
+            self.pushButton_7.clicked.connect(self.get_video_data)
+            self.pushButton_12.clicked.connect(self.download_video)
+            self.pushButton_6.clicked.connect(self.save_browse_yt)
+            self.pushButton_14.clicked.connect(self.playlist_downloader)
+            self.pushButton_13.clicked.connect(self.save_browser_playlist)
 
-    def handle_progress(self, block_num, block_size, total_size):
-        read_data = block_num * block_size
-        if total_size > 0:
-            download_percentage = read_data * 100 / total_size
-            self.progressBar.setValue(download_percentage)
-            QApplication.processEvents()
+        def handle_progress(self, block_num, block_size, total_size):
+            read_data = block_num * block_size
+            if total_size > 0:
+                download_percentage = read_data * 100 / total_size
+                self.progressBar.setValue(download_percentage)
+                QApplication.processEvents()
 
-    def handle_browse(self):
-        save_location = QFileDialog.getSaveFileName(caption='Save as', directory='.', filter="All Files(*.*)")
-        print(save_location)
-        self.lineEdit_2.setText(str(save_location[0]))
+        def handle_browse(self):
+            save_location = QFileDialog.getSaveFileName(caption='Save as', directory='.', filter="All Files(*.*)")
+            print(save_location)
+            self.lineEdit_2.setText(str(save_location[0]))
 
-    def download_file(self):
-        print('Stating Download')
-        download_url = self.lineEdit.text()
-        save_location = self.lineEdit_2.text()
+        def download_file(self):
+            print('Stating Download')
+            download_url = self.lineEdit.text()
+            save_location = self.lineEdit_2.text()
 
-        if download_url == '' or save_location == '':
-            QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
-        else:
-            try:
-                urllib.request.urlretrieve(download_url, save_location, self.handle_progress)
-            except Exception:
+            if download_url == '' or save_location == '':
                 QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
-        QMessageBox.information(self, "Download Completed", "The Download Completed Successfully")
-        self.lineEdit.setText('')
-        self.lineEdit_2.setText('')
-        self.progressBar.setValue(0)
-
-    def save_browse(self):
-        pass
-
-    #####################YT downloader##############################
-
-    def save_browse_yt(self):
-        save_location = QFileDialog.getSaveFileName(caption='Save as', directory='.', filter="All Files(*.*)")
-        self.lineEdit_8.setText(str(save_location[0]))
-
-    def get_video_data(self):
-        video_url = self.lineEdit_7.text()
-        if video_url == '':
-            QMessageBox.warning(self, "Data Error", "Provide a Valid Video URL")
-        else:
-            video = pafy.new(video_url)
-            print(video.title)
-            print(video.duration)
-
-            video_streams = video.videostreams
-            for stream in video_streams:
-                size = humanize.naturalsize(stream.get_filesize())
-                if stream.extension == 'mp4':
-                    data = "{} {} {} {}".format(stream.mediatype, stream.extension, stream.quality, size)
-                    self.comboBox.addItem(data)
-
-    def download_video(self):
-        video_url = self.lineEdit_7.text()
-        save_location = self.lineEdit_8.text()
-        if video_url == '' or save_location == '':
-            QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
-        else:
-            video = pafy.new(video_url)
-            video_stream = video.videostreams
-            video_quality = self.comboBox.currentIndex()
-            best_quality = video.getbest()
-            download = best_quality.download(filepath=save_location, callback=self.video_progress)
+            else:
+                try:
+                    urllib.request.urlretrieve(download_url, save_location, self.handle_progress)
+                except Exception:
+                    QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
             QMessageBox.information(self, "Download Completed", "The Download Completed Successfully")
+            self.lineEdit.setText('')
+            self.lineEdit_2.setText('')
+            self.progressBar.setValue(0)
+
+        def save_browse(self):
+            pass
+
+        #####################YT downloader##############################
+
+        def save_browse_yt(self):
+            save_location = QFileDialog.getSaveFileName(caption='Save as', directory='.', filter="All Files(*.*)")
+            self.lineEdit_8.setText(str(save_location[0]))
+
+        def get_video_data(self):
+            video_url = self.lineEdit_7.text()
+            if video_url == '':
+                QMessageBox.warning(self, "Data Error", "Provide a Valid Video URL")
+            else:
+                video = pafy.new(video_url)
+                print(video.title)
+                print(video.duration)
+
+                video_streams = video.videostreams
+                for stream in video_streams:
+                    size = humanize.naturalsize(stream.get_filesize())
+                    if stream.extension == 'mp4':
+                        data = "{} {} {} {}".format(stream.mediatype, stream.extension, stream.quality, size)
+                        self.comboBox.addItem(data)
+
+        def download_video(self):
+            video_url = self.lineEdit_7.text()
+            save_location = self.lineEdit_8.text()
+            if video_url == '' or save_location == '':
+                QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
+            else:
+                video = pafy.new(video_url)
+                video_stream = video.videostreams
+                video_quality = self.comboBox.currentIndex()
+                best_quality = video.getbest()
+                download = best_quality.download(filepath=save_location, callback=self.video_progress)
+                QMessageBox.information(self, "Download Completed", "The Download Completed Successfully")
+                QApplication.processEvents()
+
+        def video_progress(self, total, received, ratio, rate, time):
+            read_data = received
+            if total > 0:
+                download_percentage = read_data * 100 / total
+                self.progressBar_3.setValue(download_percentage)
+                remaining_time = round(time / 60, 2)
+                self.label_8.setText(str('{} minutes remaining'.format(remaining_time)))
+                QApplication.processEvents()
+
+        def playlist_downloader(self):
+            playlist_url = self.lineEdit_13.text()
+            save_location = self.lineEdit_14.text()
+            if playlist_url == '' or save_location == '':
+                QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
+            else:
+                playlist = pafy.get_playlist(playlist_url)
+                playlist_videos = playlist['items']
+                self.lcdNumber_2.display(len(playlist_videos))
+
+            os.chdir(save_location)
+            if os.path.exists(str(playlist['title'])):
+                os.chdir(str(playlist['title']))
+            else:
+                os.mkdir(str(playlist['title']))
+                os.chdir(str(playlist['title']))
+            current_video_in_download = 1
+            quality = self.comboBox_2.currentIndex()
             QApplication.processEvents()
 
-    def video_progress(self, total, received, ratio, rate, time):
-        read_data = received
-        if total > 0:
-            download_percentage = read_data * 100 / total
-            self.progressBar_3.setValue(download_percentage)
-            remaining_time = round(time / 60, 2)
-            self.label_8.setText(str('{} minutes remaining'.format(remaining_time)))
-            QApplication.processEvents()
+            for video in playlist_videos:
+                video = video['pafy']
+                best_quality = video.getbest()
+                self.lcdNumber.display(current_video_in_download)
+                download = best_quality.download(callback=self.playlist_progress)
+                QApplication.processEvents()
+                current_video_in_download += 1
 
-    def playlist_downloader(self):
-        playlist_url = self.lineEdit_13.text()
-        save_location = self.lineEdit_14.text()
-        if playlist_url == '' or save_location == '':
-            QMessageBox.warning(self, "Data Error", "Provide a Valid URL or save Location")
-        else:
-            playlist = pafy.get_playlist(playlist_url)
-            playlist_videos = playlist['items']
-            self.lcdNumber_2.display(len(playlist_videos))
+        def playlist_progress(self, total, received, ratio, rate, time):
+            read_data = received
+            if total > 0:
+                download_percentage = read_data * 100 / total
+                self.progressBar_4.setValue(download_percentage)
+                remaining_time = round(time / 60, 2)
+                self.label_2.setText(str('{} minutes remaining'.format(remaining_time)))
+                QApplication.processEvents()
 
-        os.chdir(save_location)
-        if os.path.exists(str(playlist['title'])):
-            os.chdir(str(playlist['title']))
-        else:
-            os.mkdir(str(playlist['title']))
-            os.chdir(str(playlist['title']))
-        current_video_in_download = 1
-        quality = self.comboBox_2.currentIndex()
-        self.lcdNumber.display(current_video_in_download)
-        QApplication.processEvents()
+        def save_browser_playlist(self):
+            playlist_save_location = QFileDialog.getExistingDirectory(self, 'Select Download Directory')
+            self.lineEdit_14.setText(playlist_save_location)
 
-        for video in playlist_videos:
-            current_video = video['pafy']
-            current_video_stream = current_video.videostreams
-            download = current_video_stream[quality].download(callback=self.playlist_progress)
-            QApplication.processEvents()
-            current_video_in_download += 1
+    def main():
+        app = QApplication(sys.argv)
+        window = MainApp()
+        window.show()
+        app.exec_()
 
-    def playlist_progress(self, total, received, ratio, rate, time):
-        read_data = received
-        if total > 0:
-            download_percentage = read_data * 100 / total
-            self.progressBar_4.setValue(download_percentage)
-            remaining_time = round(time / 60, 2)
-            self.label_2.setText(str('{} minutes remaining'.format(remaining_time)))
-            QApplication.processEvents()
-
-    def save_browser_playlist(self):
-        playlist_save_location = QFileDialog.getExistingDirectory(self, 'Select Download Directory')
-        self.lineEdit_14.setText(playlist_save_location)
+    if __name__ == '__main__':
+        main()
 
 
-def main():
-    app = QApplication(sys.argv)
-    window = MainApp()
-    window.show()
-    app.exec_()
-
-
-if __name__ == '__main__':
-    main()
+open_idm()
--- Project Code/presentation.py
+++ Project Code/presentation.py
@@ -3,82 +3,47 @@ from selenium import webdriver
 import win32com.client
 import time
 from pynput.keyboard import Key, Controller
-# import speech_recognition as sr
 
-keyboard = Controller()
+def run_presentation():
+    keyboard = Controller()
 
+    def change_voice(a):
+        if a == 1:
+            serena.setProperty('voice', voices[1].id)
+        else:
+            serena.setProperty('voice', voices[0].id)
 
-def change_voice(a):
-    if a == 1:
-        serena.setProperty('voice', voices[1].id)
-    else:
-        serena.setProperty('voice', voices[0].id)
+    def screen_recording_start():
+        keyboard.press(Key.cmd)
+        keyboard.press('g')
+        keyboard.release(Key.cmd)
+        keyboard.release('g')
+        keyboard.press(Key.cmd)
+        keyboard.press('g')
+        keyboard.release(Key.cmd)
+        keyboard.release('g')
+        time.sleep(3)
 
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.alt)
+        keyboard.press('r')
+        screen_recording_stop()
+        time.sleep(3)
 
-def screen_recording_start():
-    keyboard.press(Key.cmd)
-    keyboard.press('g')
-    keyboard.release(Key.cmd)
-    keyboard.release('g')
-    keyboard.press(Key.cmd)
-    keyboard.press('g')
-    keyboard.release(Key.cmd)
-    keyboard.release('g')
-    time.sleep(3)
+    def screen_recording_stop():
+        keyboard.release('r')
+        keyboard.release(Key.alt)
+        keyboard.release(Key.cmd)
 
-    keyboard.press(Key.cmd)
-    keyboard.press(Key.alt)
-    keyboard.press('r')
-    screen_recording_stop()
-    time.sleep(3)
+    serena = pyttsx3.init()
+    voices = serena.getProperty('voices')
+    serena.setProperty('voice', voices[0].id)
+    serena.setProperty('rate', 170)
 
+    def my_command():
+        return str(input()).lower()
 
-def screen_recording_stop():
-    keyboard.release('r')
-    keyboard.release(Key.alt)
-    keyboard.release(Key.cmd)
-
-
-serena = pyttsx3.init()
-voices = serena.getProperty('voices')
-serena.setProperty('voice', voices[0].id)
-serena.setProperty('rate', 170)
-
-
-#
-# def my_command():
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         r.pause_threshold = 2
-#         r.adjust_for_ambient_noise(source, duration=1)
-#         audio = r.listen(source)
-#     try:
-#         command = r.recognize_google(audio)
-#         print(command)
-#         return command
-#
-#     except sr.UnknownValueError or TypeError:
-#         assistant()
-def my_command():
-    return str(input()).lower()
-
-
-def assistant():
-    command = my_command()
-    if 'open facebook' in command:
-        serena.say('Opening Facebook')
-        serena.runAndWait()
-        driver = webdriver.Firefox(executable_path='C:\geckodriver.exe')
-        driver.get('https://www.facebook.com/')
-    if 'hello serena' in command:
-        change_voice(1)
-        serena.say('Welcome Back Sir, How are you.')
-        serena.runAndWait()
-    if 'hello jarvis' in command:
-        change_voice(0)
-        serena.say('Welcome back sir, I am jarvis.')
-        serena.runAndWait()
-    if 'run' in command:
+    def assistant():
         n = 2
         with open('txt files/intro.txt', 'r') as inputFile:
             lines = inputFile.readlines()
@@ -147,14 +112,9 @@ def assistant():
         screen_recording_start()
         presentation.Close()
 
-    # else:
-    #     change_voice('s')
-    #     serena.say('You said: {}'.format(command))
-    #     serena.runAndWait()
-
-
-while True:
-    try:
-        assistant()
-    except TypeError:
-        continue
+    while True:
+        try:
+            assistant()
+        except TypeError:
+            continue
+run_presentation()
